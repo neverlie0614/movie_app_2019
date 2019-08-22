3690b6313038f95c7a88a442c57f83c023b6f596
JH <neverlie@naver.com>
2019-08-22 21:36:07 +0900

    Updates

---
+++ precache-manifest.edfdff8927cd29206dc928cfda904247.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "406a4517f911b8ef228f0112d57a4cbe",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "b0936d518b8a8677d873",
+    "url": "/movie_app_2019/static/css/main.ad0b025d.chunk.css"
+  },
+  {
+    "revision": "3cc7f05352d8eadd290c",
+    "url": "/movie_app_2019/static/js/2.6e87735a.chunk.js"
+  },
+  {
+    "revision": "b0936d518b8a8677d873",
+    "url": "/movie_app_2019/static/js/main.101ff835.chunk.js"
+  },
+  {
+    "revision": "5a2b9365f7ca01d151d6",
+    "url": "/movie_app_2019/static/js/runtime~main.64ac130e.js"
+  }
+]);
